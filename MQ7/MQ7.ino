649efe72075b06031767402426d8ee7fb5b37916
igmoreira <igmoreira.dev@gmail.com>
2018-06-05 21:48:01 -0300

    first commit

---
+++ MQ7/MQ7.ino
@@ -0,0 +1,19 @@
+#define SENSOR_PIN 15
+
+
+void setup() {
+  pinMode(SENSOR_PIN, INPUT);  
+  Serial.begin(9600);
+}
+
+void loop() {
+    int sensorValue = analogRead(SENSOR_PIN);
+
+    float sensor_voltage = sensorValue * ( 5.0 / 1024.0 );
+    float RS_gas = (5.0 - sensor_voltage) / sensor_voltage;
+    float ppm = (19.32 * pow(RS_gas, -0.64));
+    
+//    Serial.print("Concentration = ");
+    Serial.println(ppm);
+    delay(1000);
+}
